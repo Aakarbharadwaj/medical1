45952c1678fa408c787b9b711193a1e2831bd763
Aakarbharadwaj <104188303+Aakarbharadwaj@users.noreply.github.com>
2024-04-15 19:28:03 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes,Route, Router } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Router>
+      <Route />
+    </Router>
+    </>
+  )
+}
+
+export default App
